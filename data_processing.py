98b4b4fa2712b2913db88f1e3efc993fb1c68255
illGEHIRN <illmrx@proton.me>
2026-07-23 14:51:12 +0200

    feat: finished data processing, moving on to standardizing and restructuring the code base

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,4 @@
 import json
-
 import numpy as np
 import pandas as pd
 
@@ -34,6 +33,8 @@ print(df_attendance["type"].value_counts())
 
 # delete "arrival" type, its obsolete
 df_attendance = df_attendance[df_attendance["type"] != "arrival"].copy()
+# delete archived users
+df_users = df_users[~df_users["archived"]].copy()
 
 # merge two homeoffice types
 df_attendance["type"] = df_attendance["type"].replace(
@@ -57,19 +58,32 @@ raw_duration = round(((time_to - time_from).dt.total_seconds() / 3600), 1)
 # create dummies
 is_working = df_attendance["type"].isin(working_types)
 is_pause = df_attendance["type"] == "pause"
+is_doctor = df_attendance["note"] == "Lékař"
+is_training = df_attendance["note"] == "Školení "
 is_unclosed = df_attendance["from"].notna() & df_attendance["to"].isna()
+is_homeoffice = df_attendance["type"] == "homeoffice"
+is_sick = df_attendance["type"] == "sick"
 
 # working hours (for actual work types)
 df_attendance["hours_worked"] = np.where(
-    is_working & ~is_unclosed, raw_duration.fillna(0), 0.0
+    is_working & ~is_unclosed,
+    raw_duration.fillna(0), 
+    0.0
 )
 
 # calculate pause hours
-df_attendance["hours_break"] = np.where(
-    is_pause & ~is_unclosed, raw_duration.fillna(0), 0.0
+df_attendance["hours_paused"] = np.where(
+    is_pause & ~is_unclosed,
+    raw_duration.fillna(0),
+    0.0
 )
 
-conditions = [is_unclosed & is_working, is_unclosed & is_pause, is_pause, ~is_working]
+conditions = [is_unclosed & is_working,
+              is_unclosed & is_pause,
+              is_pause, 
+              ~is_working,
+              ]
+
 choices = [
     "Unclosed Work Shift",
     "Unclosed Break",
@@ -78,16 +92,64 @@ choices = [
 ]
 
 df_attendance["status_flag"] = np.select(
-    conditions, choices, default="Standard Completed Shift"
+    conditions, 
+    choices,
+    default="Standard Completed Shift"
 )
 
+df_attendance["Doctor"] = is_doctor
+df_attendance["Training"] = is_training
+df_attendance["SickLeave"] = is_sick
 
-# -----------------------------------------------------------------------------------------
 
 # Datetime, Durations Formatting and Cleaning
 df_attendance["date"] = pd.to_datetime(
-    df_attendance["date"]
-).dt.date  # "Date" is the exact day the attendance is occuring not when its set or updated so "Date" with type "Vacation" means that on that specific date a vacation occured
+    df_attendance["date"]).dt.date  # "Date" is the exact day the attendance is occuring not when its set or updated so "Date" with type "Vacation" means that on that specific date a vacation occured
+
+
+# keys, join 
+df_dim_users = df_users.rename(columns={
+    "id": "user_id"
+})
+df_attendance = df_attendance.rename(columns={
+    "user": "user_id"
+})
+
+
+# select clean needed cols
+attendance_cols = [
+    "id", "user_id", "date", "type", "note", "status_flag", "from", "to",
+    "hours_worked", "hours_paused", "lunch", "manual", "wage"
+]
+
+df_attendance = df_attendance[attendance_cols]
+
+# keep only valid type subcategories ("note")
+valid_notes = [
+    "Schůzka", "Lékař", "Osobní důvody", "Pořádání akce", "Školení", 
+    "Doprovod osoby blízké k lékaři", "Stáž v zahraničí (přidáno skriptem)", 
+    "Škola", "Doprovod dítěte", "Čerpání zbytku dovolené"
+]
+
+# logic to drop invalid pause subcategories ("note"s) and keep all rows with other type than "pause" regardless of "note" status
+is_valid_pause = (df_attendance["type"] == "pause") & (df_attendance["note"].isin(valid_notes))
+is_other_type  = df_attendance["type"] != "pause"
+
+df_attendance = df_attendance[is_other_type | is_valid_pause].copy()
+
+
+
+
+# export to csv
+df_dim_users.to_csv("CSVs/dim_users.csv", index=False, encoding="utf-8-sig")
+df_attendance.to_csv("CSVs/fact_attendance.csv", index=False, encoding="utf-8-sig")
+
+
+
+
+
+
+#################################################################################################
 
 
 # Filter for records where 'from' exists, but 'to' is missing (NaN/None)
@@ -98,3 +160,8 @@ missing_to_with_from = df_attendance[
 print(f"Total rows where 'from' exists but 'to' is NA: {len(missing_to_with_from)}")
 print("\n--- Breakdown by Type ---")
 print(missing_to_with_from["type"].value_counts(dropna=False))
+
+
+
+df_attendance["note"].value_counts()
+df_attendance["type"].value_counts()
